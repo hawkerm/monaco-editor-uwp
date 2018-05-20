8e8d198564d61f06ded2382f202f233f97e32f79
groot <questions@mikeware.com>
2018-05-20 16:23:31 -0700

    Clean-up ILanguageExtensionPoint

--- MonacoEditorComponent/Monaco/Languages/ILanguageExtensionPoint.cs
+++ MonacoEditorComponent/Monaco/Languages/ILanguageExtensionPoint.cs
@@ -11,49 +11,25 @@ namespace Monaco.Languages
     /// <summary>
     /// Object Parser for https://microsoft.github.io/monaco-editor/api/interfaces/monaco.languages.ilanguageextensionpoint.html
     /// </summary>
-    public sealed class ILanguageExtensionPoint // TODO: Convert to Interface
+    public sealed class ILanguageExtensionPoint
     {
         #pragma warning disable CS1591
         [JsonProperty("id")]
-        public string Id { get; private set; }
+        public string Id { get; set; }
         [JsonProperty("aliases")]
-        public string[] Aliases { get; private set; }
+        public string[] Aliases { get; set; }
         [JsonProperty("configuration")]
-        public string Configuration { get; private set; }
+        public string Configuration { get; set; }
         [JsonProperty("extensions")]
-        public string[] Extensions { get; private set; }
+        public string[] Extensions { get; set; }
         [JsonProperty("filenames")]
-        public string[] Filenames { get; private set; }
+        public string[] Filenames { get; set; }
         [JsonProperty("filenamePatterns")]
-        public string[] FilenamePatterns { get; private set; }
+        public string[] FilenamePatterns { get; set; }
         [JsonProperty("firstLine")]
-        public string FirstLine { get; private set; }        
+        public string FirstLine { get; set; }        
         [JsonProperty("mimetypes")]
-        public string[] Mimetypes { get; private set; }
-        
-        [Obsolete("TODO: Use JsonConvert Instead.")]
-        public static ILanguageExtensionPoint Create(IJsonValue languageValue)
-        {
-            ILanguageExtensionPoint language = new ILanguageExtensionPoint();
-            var jsonObject = languageValue.GetObject();
-
-            language.Id = jsonObject.GetNamedString("id");
-            language.Aliases = jsonObject.GetNamedArray("aliases", new JsonArray()).Select(value => value.GetString()).ToArray();
-            if (jsonObject.ContainsKey("configuration"))
-            {
-                language.Configuration = jsonObject.GetNamedString("configuration", string.Empty);
-            }
-            language.Extensions = jsonObject.GetNamedArray("extensions", new JsonArray()).Select(value => value.GetString()).ToArray();
-            language.Filenames = jsonObject.GetNamedArray("filenames", new JsonArray()).Select(value => value.GetString()).ToArray();
-            language.FilenamePatterns = jsonObject.GetNamedArray("filenamePatterns", new JsonArray()).Select(value => value.GetString()).ToArray();
-            if (jsonObject.ContainsKey("firstLine"))
-            {
-                language.FirstLine = jsonObject.GetNamedString("firstLine", string.Empty);
-            }
-            language.Mimetypes = jsonObject.GetNamedArray("mimetypes", new JsonArray()).Select(value => value.GetString()).ToArray();
-
-            return language;
-        }
+        public string[] Mimetypes { get; set; }        
         #pragma warning restore CS1591
     }
 }
